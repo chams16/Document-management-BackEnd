0fbf78b9502ade5fd6d02a084e95d9cd59333e96
chams16 <chamseddine.bahri@eniso.u-sousse.tn>
2023-02-10 23:56:10 +0100

    spring boot project

---
+++ src/main/java/com/GED/GED/GedApplication.java
@@ -0,0 +1,13 @@
+package com.GED.GED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedApplication.class, args);
+	}
+
+}
